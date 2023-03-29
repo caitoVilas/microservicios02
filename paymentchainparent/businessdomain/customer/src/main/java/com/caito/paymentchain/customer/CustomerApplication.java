fc78226c80893e36b1468880448cce49adf2cd88
Claudio Vilas <claudio.vilas@eldars.local>
2023-03-29 16:51:45 -0300

    creada arquitectura

--- paymentchainparent/businessdomain/customer/src/main/java/com/caito/paymentchain/customer/CustomerApplication.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/caito/paymentchain/customer/CustomerApplication.java
@@ -2,7 +2,10 @@ package com.caito.paymentchain.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +13,12 @@ public class CustomerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	WebClient.Builder loadBalancedWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 }
